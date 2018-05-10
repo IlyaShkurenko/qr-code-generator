68b17c3f5f16581a79ed0ea907c5edfb1a6c78b9
illiashkurenko <ilya-shkurenko@mail.ru>
2018-05-10 16:24:34 +0300

    restart

--- docs/dist/6.build.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([6],{30:function(e,t,n){"use strict";var r=function(){var e=this,t=e.$createElement;return(e._self._c||t)("div",[e._t("header"),e._v(" "),e._t("content"),e._v(" "),e._t("footer")],2)},l=[],u={render:r,staticRenderFns:l};t.a=u},6:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r=n(30),l=n(3),u=l(null,r.a,!1,null,null,null);t.default=u.exports}});
-//# sourceMappingURL=6.build.js.map
---
+++ docs/static/js/0.cb5b24f28027c46a5e05.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],{niH5:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var l={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._t("settings",null,{header:"Settings"})],2)},staticRenderFns:[]},s=n("VU/8")(null,l,!1,null,null,null);t.default=s.exports}});
+//# sourceMappingURL=0.cb5b24f28027c46a5e05.js.map
